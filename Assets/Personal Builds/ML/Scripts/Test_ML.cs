be6ba396364424a1b58e1a19ef3cd566b0b448a1
joechip125 <52743358+joechip125@users.noreply.github.com>
2022-03-01 14:53:17 +0100

    Update Test_ML.cs

--- Assets/Personal Builds/ML/Scripts/Test_ML.cs
+++ Assets/Personal Builds/ML/Scripts/Test_ML.cs
@@ -7,6 +7,8 @@ public class Test_ML : MonoBehaviour
 
     [SerializeField] private GameObject TileTest;
     private List<Vector3> tilePos = new List<Vector3>();
+    private Vector3 currentTileSize;
+    private Vector3 currentPosition;
 
     private Vector3 GetMeshSize(GameObject theObject)
     {
@@ -15,20 +17,29 @@ public class Test_ML : MonoBehaviour
 
     void Start()
     {
-       CreateTileRow();
+       CreatTileSquare(new Vector2(8, 7));
     }
 
-    private void CreateTileRow()
+    private void CreatTileSquare(Vector2 tileCount)
     {
-        var testTile = Instantiate(TileTest);
-        var size = testTile.GetComponent<MeshFilter>().mesh.bounds.size.x;
-        var currentPos = testTile.transform.position;
-
-        for (int i = 0; i < 8; i++)
+        var startVector = transform.position;
+        currentPosition = startVector;
+        for (int i = 0; i < tileCount.y; i++)
         {
-            testTile = Instantiate(TileTest);
-            testTile.transform.position += currentPos + new Vector3(size, 0);
-            currentPos = testTile.transform.position;
+            CreateTileRow((int)tileCount.x);
+            currentPosition = startVector
+                              + new Vector3(0, 0, currentTileSize.z * i);
+        }
+    }
+
+    private void CreateTileRow(int rowCount)
+    {
+        for (int i = 0; i < rowCount; i++)
+        {
+            var testTile = Instantiate(TileTest);
+            currentTileSize = testTile.GetComponent<MeshFilter>().mesh.bounds.size;
+            testTile.transform.position = currentPosition + new Vector3(currentTileSize.x, 0);
+            currentPosition = testTile.transform.position;
         }
     }
 
