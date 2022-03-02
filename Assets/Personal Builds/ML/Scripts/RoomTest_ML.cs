d09eb9112a43e218e3c9c949dfe67c7b7875fc41
joechip125 <52743358+joechip125@users.noreply.github.com>
2022-03-02 10:04:28 +0100

    reset transforms for prefabs

--- Assets/Personal Builds/ML/Scripts/RoomTest_ML.cs
+++ Assets/Personal Builds/ML/Scripts/RoomTest_ML.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class RoomTest_ML : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject testSpawn;
     void Start()
     {
-        
+        GetFloorTiles();
     }
 
-    // Update is called once per frame
+    private void GetFloorTiles()
+    {
+       var tiles = gameObject.GetComponentsInChildren<MeshFilter>()
+            .Where(x => x.CompareTag("Floor")).ToList();
+
+       for (int i = 0; i < 5; i++)
+       {
+
+           var rand = Random.Range(0, tiles.Count);
+           var tile = tiles[rand].transform;
+           var pos = tiles[rand].gameObject.transform.TransformPoint(tile.position);
+           var tempVase = Instantiate(testSpawn, tile.position, Quaternion.identity);
+       }
+    }
+
     void Update()
     {
-        
+
     }
 }
