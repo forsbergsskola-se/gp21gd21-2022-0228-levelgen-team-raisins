31bed2fea23b8078d51b63cbed1be5713b7a4a25
Partisan Programmer <oliver.kallerfors@student.forsbergsskola.se>
2022-03-02 20:33:23 +0100

    Added Scriptable object position and position updater

---
+++ Assets/Personal Builds/OK/Scripts_OK/PositionSO.cs
@@ -0,0 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Position", menuName = "Essentials/Transform/Position")]
+public class PositionSO : ScriptableObject{
+    public Vector3 position;
+}
