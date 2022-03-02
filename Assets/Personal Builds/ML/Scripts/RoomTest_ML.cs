abb7b460bd2f8887d21b4683f3a461cd11f95afd
joechip125 <52743358+joechip125@users.noreply.github.com>
2022-03-02 11:06:10 +0100

    working on spawning assets randomly

--- Assets/Personal Builds/ML/Scripts/RoomTest_ML.cs
+++ Assets/Personal Builds/ML/Scripts/RoomTest_ML.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using UnityEngine;
 
@@ -16,15 +17,40 @@ public class RoomTest_ML : MonoBehaviour
        var tiles = gameObject.GetComponentsInChildren<MeshFilter>()
             .Where(x => x.CompareTag("Floor")).ToList();
 
+       List<int> randoms = new List<int>();
+
+       for (int i = 0; i < 23; i++)
+       {
+           var randomNumbers = Random.Range(0, tiles.Count);
+           if(!randoms.Contains(randomNumbers))
+               randoms.Add(randomNumbers);
+       }
+
+
        for (int i = 0; i < 5; i++)
        {
-           var rand = Random.Range(0, tiles.Count);
-           var tile = tiles[rand].gameObject.transform;
+           var rand = randoms[i];
+           var randRot = Random.Range(5f, 300f);
+           var tilePos = tiles[rand].gameObject.transform.position;
            var scale = tiles[rand].mesh.bounds.size;
-           var adjustPos = tile.position + new Vector3(scale.x / 2,0, -scale.z / 2);
-           Debug.Log(tile.position);
+           var adjustPos = tilePos + new Vector3(scale.x / 2,0, -scale.z / 2);
            var tempVase = Instantiate(testSpawn, adjustPos, Quaternion.identity);
+
+           AdjustObjectPlacement(tempVase, 300f);
        }
     }
+
+    private void AdjustObjectPlacement(GameObject theObject, float maxRotation)
+    {
+        var randRot = Random.Range(0f, maxRotation);
+        theObject.transform.Rotate(new Vector3(0,1,0), randRot);
+
+        var objectSize = theObject.GetComponent<MeshFilter>().mesh.bounds.size;
+        var randMoveX = Random.Range(0f, objectSize.x / 2);
+        var randMoveZ = Random.Range(0f, objectSize.z / 2);
+
+        theObject.transform.position += new Vector3(randMoveX, 0, randMoveZ);
+    }
+
 
 }
