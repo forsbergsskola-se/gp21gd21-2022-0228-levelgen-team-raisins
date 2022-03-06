d09c0b9bb3d554fab6d1461907edf66cdfcf9969
Partisan Programmer <oliver.kallerfors@student.forsbergsskola.se>
2022-03-06 23:04:08 +0100

    Added UIDifficulty Display

---
+++ Assets/_Final Game/Scripts/Difficulty Scripts/UIDifficultyUpdater.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIDifficultyUpdater : MonoBehaviour{
+    [SerializeField] GameDifficultySO gameDifficultySo;
+
+    TextMeshProUGUI textMeshProUGUI;
+    void Awake(){
+        textMeshProUGUI = GetComponent<TextMeshProUGUI>();
+    }
+
+    void OnEnable(){
+        gameDifficultySo.difficultyChangeEvent.AddListener(SetDifficultyText);
+        SetDifficultyText(gameDifficultySo.Difficulty);
+    }
+    void OnDisable(){
+        gameDifficultySo.difficultyChangeEvent.RemoveListener(SetDifficultyText);
+    }
+
+    void SetDifficultyText(Difficulty difficulty){
+        textMeshProUGUI.text = difficulty.ToString();
+    }
+
+
+}
