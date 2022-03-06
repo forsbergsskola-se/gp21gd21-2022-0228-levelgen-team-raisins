2ea49a500de814bdd369791f7d450b754c52e83f
Partisan Programmer <oliver.kallerfors@student.forsbergsskola.se>
2022-03-06 21:12:14 +0100

    Added Difficulty Script and Created Difficulty SO

---
+++ Assets/_Final Game/Scripts/Difficulty Scripts/GameDifficultySO.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum Difficulty{
+    Easy,
+    Medium,
+    Hard,
+    Nightmare
+}
+
+[CreateAssetMenu(fileName = "New Game Difficulty", menuName = "Essentials/States/Game Difficulty")]
+public class GameDifficultySO : ScriptableObject{
+
+    [System.NonSerialized] public UnityEvent<Difficulty> difficultyChangeEvent;
+    Difficulty difficulty;
+
+    public Difficulty Difficulty{
+        get => difficulty;
+        set{
+            difficulty = value;
+            //Invokes Event when Difficulty is changed
+            difficultyChangeEvent.Invoke(value);
+        }
+    }
+
+    void OnEnable(){
+        //Creates event if none exists.
+        if (difficultyChangeEvent == null){
+            difficultyChangeEvent = new UnityEvent<Difficulty>();
+        }
+
+        Difficulty = Difficulty.Easy;
+    }
+}
