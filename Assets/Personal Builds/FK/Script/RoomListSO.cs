8e9675e3aa271891ebdfca523744782e88d8e73f
Felix K <felix.kjellberg@student.forsbergsskola.se>
2022-03-07 19:22:43 +0100

    Connections now sends rooms to a list in dungeon

--- Assets/Personal Builds/FK/Script/RoomListSO.cs
+++ Assets/Personal Builds/FK/Script/RoomListSO.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Room list", menuName = "List/Room list")]
 public class RoomListSO : ScriptableObject{
     public List<Room> rooms;
+    public Room startRoom;
 
     void OnEnable(){
         rooms = new List<Room>();
+        rooms.Add(startRoom);
     }
 
     void OnDisable(){
