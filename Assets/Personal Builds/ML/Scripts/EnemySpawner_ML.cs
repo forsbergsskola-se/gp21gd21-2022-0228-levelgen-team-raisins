0a0914e53d44bef78fb2e8c82008acd20477c551
joechip125 <52743358+joechip125@users.noreply.github.com>
2022-03-08 19:42:47 +0100

    Update EnemySpawner_ML.cs

--- Assets/Personal Builds/ML/Scripts/EnemySpawner_ML.cs
+++ Assets/Personal Builds/ML/Scripts/EnemySpawner_ML.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using Unity.Multiplayer.Samples.BossRoom;
 using Unity.Multiplayer.Samples.BossRoom.Client;
 using Unity.Multiplayer.Samples.BossRoom.Server;
+
 using Unity.Netcode;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -19,15 +20,12 @@ public class EnemySpawner_ML : MonoBehaviour
     [SerializeField] private PrefabListSO NightmareEnemies;
     private List<GameObject> currentEnemies;
     public int maxEnemiesToSpawn = 5;
+
+    public float spawnInterval = 5;
+    private MyTimer spawnTimer;
+
+
     private List<Transform> spawnPoints = new List<Transform>();
-
-    public void SpawnEnemy()
-    {
-      var enemyNum =  Random.Range(0, currentEnemies.Count);
-      var spawnNum = Random.Range(0, spawnPoints.Count);
-      Instantiate(currentEnemies[enemyNum], spawnPoints[spawnNum].position, Quaternion.identity);
-    }
-
 
     public void SpawnRandomNumberEnemies()
     {
@@ -43,12 +41,19 @@ public class EnemySpawner_ML : MonoBehaviour
                 var enemyNum =  Random.Range(0, currentEnemies.Count);
                 var spawnNum = Random.Range(0, pointsToSpawn.Count);
                 var enemy = Instantiate(currentEnemies[enemyNum], pointsToSpawn[spawnNum].position, Quaternion.identity);
-             //   enemy.GetComponent<ClientCharacter>().ChildVizObject.OnNetworkSpawn();
+                //   enemy.GetComponent<ClientCharacter>().ChildVizObject.OnNetworkSpawn();
                 enemy.GetComponent<NetworkObject>().Spawn();
-              //  enemy.GetComponent<PhysicsWrapper>().OnNetworkSpawn();
-              //  enemy.GetComponent<ServerCharacter>().OnNetworkSpawn();
+                //  enemy.GetComponent<PhysicsWrapper>().OnNetworkSpawn();
+                //  enemy.GetComponent<ServerCharacter>().OnNetworkSpawn();
             }
         }
+    }
+
+    public void SpawnEnemy()
+    {
+      var enemyNum =  Random.Range(0, currentEnemies.Count);
+      var spawnNum = Random.Range(0, spawnPoints.Count);
+      Instantiate(currentEnemies[enemyNum], spawnPoints[spawnNum].position, Quaternion.identity);
     }
 
     void Start()
