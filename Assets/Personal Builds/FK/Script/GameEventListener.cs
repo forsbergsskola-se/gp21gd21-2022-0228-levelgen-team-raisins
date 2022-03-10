8e5aea9c0729ccd7747e9f39a35e82aa8a7d6d6b
Felix Kjellberg <90178381+FelixKje@users.noreply.github.com>
2022-03-10 10:22:10 +0100

    GameEvent and GameEventlistener finnished scripts

--- Assets/Personal Builds/FK/Script/GameEventListener.cs
+++ Assets/Personal Builds/FK/Script/GameEventListener.cs
@@ -1,18 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class GameEventListener : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class GameEventListener : MonoBehaviour{
+    [SerializeField] GameEventSO gameEvent;
+    [SerializeField] UnityEvent unityEvent;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Awake() => gameEvent.Register(this);
+    void OnDestroy() => gameEvent.Deregister(this);
+    public void RaiseEvent() => unityEvent.Invoke();
 }
--- Assets/Personal Builds/FK/Script/GameEventSO.cs
+++ Assets/Personal Builds/FK/Script/GameEventSO.cs
@@ -5,4 +5,13 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Game Event", fileName = "New Game Event")]
 public class GameEventSO : ScriptableObject{
     HashSet<GameEventListener> listeners = new HashSet<GameEventListener>();
+
+    public void Invoke(){
+        foreach (var globalEventListener in listeners){
+            globalEventListener.RaiseEvent();
+        }
+    }
+
+    public void Register(GameEventListener gameEventListener) => listeners.Add(gameEventListener);
+    public void Deregister(GameEventListener gameEventListener) => listeners.Remove(gameEventListener);
 }
